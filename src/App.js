83f93b64fbca807d94029a37b00592709a6b3844
Shafique <sfqtuhin04@gmail.com>
2022-04-08 22:46:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "./firebase-init";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
+const auth = getAuth(app);
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSUbmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError("Please input at least one special charectar");
+      return;
+    }
+
+    setValidated(true);
+    setError("");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    e.preventDefault();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-3 container mx-auto w-50">
+      <h2 className="text-info">Please Register!!</h2>
+      <Form noValidate validated={validated} onSubmit={handleSUbmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onBlur={handlePassBlur}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <p className="text-danger">{error}</p>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
---
+++ src/firebase-init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBShwucUCN6YT3rlf3ziTD33ZsJEFmTs9k",
+  authDomain: "email-pass-auth-ceb20.firebaseapp.com",
+  projectId: "email-pass-auth-ceb20",
+  storageBucket: "email-pass-auth-ceb20.appspot.com",
+  messagingSenderId: "618850201092",
+  appId: "1:618850201092:web:3856af57ae07308df24f51",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
